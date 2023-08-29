2266159e81ee72fcfdd9a7ee58f9f2f14a30af52
Keea <zak.owasil@gmail.com>
2023-08-29 19:20:47 +0400

    Register turned into steps WORKING (not customized yet)

--- resources/js/register.js
+++ resources/js/register.js
@@ -1,49 +1,74 @@
 import '../css/register.css';
 
-// Get all the step cards and buttons
-const stepCards = document.querySelectorAll('.step');
-const nextButtons = document.querySelectorAll('.btn-outline-dark');
-const previousButtons = document.querySelectorAll('.btn-info');
-
-let currentStep = 0;
-
-// Show the first step initially
-stepCards[currentStep].style.display = 'block';
-stepCards[1].style.display = 'none';
-stepCards[2].style.display = 'none';
-stepCards[3].style.display = 'none';
-stepCards[4].style.display = 'none';
-
-// Function to go to the next step
-function nextStep() {
-    if (currentStep >= 0 && currentStep <= 3) {
-        stepCards[currentStep].style.display = 'none';
-        currentStep++;
-        stepCards[currentStep].style.display = 'block';
-    }
-}
-
-// Function to go to the previous step
-function previousStep() {
-    if (currentStep >= 1 && currentStep <= 4) {
-        stepCards[currentStep].style.display = 'none';
-        currentStep--;
-        stepCards[currentStep].style.display = 'block';
-    }
-}
-
-// Add event listeners to all Next buttons
-nextButtons.forEach(button => {
-    button.addEventListener('click', function (event) {
-        event.preventDefault();
-        nextStep();
+/**
+ * Define a function to navigate betweens form steps.
+ * It accepts one parameter. That is - step number.
+ */
+const navigateToFormStep = (stepNumber) => {
+    /**
+     * Hide all form steps.
+     */
+    document.querySelectorAll(".form-step").forEach((formStepElement) => {
+        formStepElement.classList.add("d-none");
     });
-});
-
-// Add event listeners to all Previous buttons
-previousButtons.forEach(button => {
-    button.addEventListener('click', function (event) {
-        event.preventDefault();
-        previousStep();
+    /**
+     * Mark all form steps as unfinished.
+     */
+    document.querySelectorAll(".form-stepper-list").forEach((formStepHeader) => {
+        formStepHeader.classList.add("form-stepper-unfinished");
+        formStepHeader.classList.remove("form-stepper-active", "form-stepper-completed");
+    });
+    /**
+     * Show the current form step (as passed to the function).
+     */
+    document.querySelector("#step-" + stepNumber).classList.remove("d-none");
+    /**
+     * Select the form step circle (progress bar).
+     */
+    const formStepCircle = document.querySelector('li[step="' + stepNumber + '"]');
+    /**
+     * Mark the current form step as active.
+     */
+    formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-completed");
+    formStepCircle.classList.add("form-stepper-active");
+    /**
+     * Loop through each form step circles.
+     * This loop will continue up to the current step number.
+     * Example: If the current step is 3,
+     * then the loop will perform operations for step 1 and 2.
+     */
+    for (let index = 0; index < stepNumber; index++) {
+        /**
+         * Select the form step circle (progress bar).
+         */
+        const formStepCircle = document.querySelector('li[step="' + index + '"]');
+        /**
+         * Check if the element exist. If yes, then proceed.
+         */
+        if (formStepCircle) {
+            /**
+             * Mark the form step as completed.
+             */
+            formStepCircle.classList.remove("form-stepper-unfinished", "form-stepper-active");
+            formStepCircle.classList.add("form-stepper-completed");
+        }
+    }
+};
+/**
+ * Select all form navigation buttons, and loop through them.
+ */
+document.querySelectorAll(".btn-navigate-form-step").forEach((formNavigationBtn) => {
+    /**
+     * Add a click event listener to the button.
+     */
+    formNavigationBtn.addEventListener("click", () => {
+        /**
+         * Get the value of the step.
+         */
+        const stepNumber = parseInt(formNavigationBtn.getAttribute("step_number"));
+        /**
+         * Call the function to navigate to the target form step.
+         */
+        navigateToFormStep(stepNumber);
     });
 });
--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,112 +1,182 @@
 @extends('app')
 
 @section('content')
-    <!-- Registering with steps for better user experience -->
-    <h2 class="register_header">Register</h2>
+    <div>
 
-    <form id="registration_form" action="{{ route('register') }}" method="POST">
+        <!-- Registering with steps for better user experience -->
+        {{-- credits for template: https://levelup.gitconnected.com/create-a-multi-step-form-using-html-css-and-javascript-30aca5c062fc --}}
 
-        @error('username')
-            <div class="errormsg">
-                {{ $message }}
-            </div>
-        @enderror
-        @error('email')
-            <div class="errormsg">
-                {{ $message }}
-            </div>
-        @enderror
-        @error('password')
-            <div class="errormsg">
-                {{ $message }}
-            </div>
-        @enderror
+        <h2 class="register_header">Register</h2>
 
-        @csrf
+        <div id="multi-step-form-container">
 
-         <!-- Step 1: choose registering type -->
-        <div class="step card" id="Registering_Type_Content">
-            <h3>Step 1 out of 4: Select registering type</h3>
+            <!-- Form Steps / Progress Bar -->
+            <ul class="form-stepper form-stepper-horizontal">
+                <!-- Step 1 -->
+                <li class="form-stepper-active form-stepper-list" step="1">
+                    <a>
+                        <span class="form-stepper-circle">
+                            <span>1</span>
+                        </span>
+                        <div>Registering Type</div>
+                    </a>
+                </li>
+                <!-- Step 2 -->
+                <li class="form-stepper-unfinished form-stepper-list" step="2">
+                    <a>
+                        <span class="form-stepper-circle text-muted">
+                            <span>2</span>
+                        </span>
+                        <div class="text-muted">Basic Account Details</div>
+                    </a>
+                </li>
+                <!-- Step 3 -->
+                <li class="form-stepper-unfinished form-stepper-list" step="3">
+                    <a>
+                        <span class="form-stepper-circle text-muted">
+                            <span>3</span>
+                        </span>
+                        <div class="text-muted">Personal Details</div>
+                    </a>
+                </li>
+                <!-- Step 4 -->
+                <li class="form-stepper-unfinished form-stepper-list" step="4">
+                    <a>
+                        <span class="form-stepper-circle text-muted">
+                            <span>4</span>
+                        </span>
+                        <div class="label text-muted">Location Details</div>
+                    </a>
+                </li>
+                <!-- Confirmation -->
+                <li class="form-stepper-unfinished form-stepper-list" step="5">
+                    <a>
+                        <span class="form-stepper-circle text-muted">
+                            <span>5</span>
+                        </span>
+                        <div class="text-muted">Confirmation</div>
+                    </a>
+                </li>
+            </ul>
 
-            <div class="d-flex">
-                <div>
-                    <p>I want to bid on items!</p>
-                    <input type="radio" class="radio_item" value="bidder" name="usertype" id="usertype_bidder">
-                    <label class="label_item gentle-hover-shake" for="usertype_bidder"> <i
-                            class="fa-solid fa-bag-shopping"></i> </label>
-                </div>
-                <div>
-                    <p>I have items to sell!</p>
-                    <input type="radio" class="radio_item" value="merchant" name="usertype" id="usertype_merchant">
-                    <label class="label_item gentle-hover-shake" for="usertype_merchant"> <i class="fa-solid fa-tag"></i>
-                    </label>
-                </div>
-                <div>
-                    <p>I want to expertise items!</p>
-                    <input type="radio" class="radio_item" value="auctioneer" name="usertype" id="usertype_auctioneer">
-                    <label class="label_item gentle-hover-shake" for="usertype_auctioneer"> <i
-                            class="fa-solid fa-user-tie"></i> </label>
-                </div>
-            </div>
 
-            <button class="btn btn-outline-dark">Next</button>
-        </div> 
 
-        <!-- Step 2: account information -->
-        <div class="step card" id="Account_Information_Content">
-            <h3>Step 2 out of 4: Add account details</h3>
-            <label>Enter a username</label>
-            <input type="text" name="username">
-            <label>Enter email</label>
-            <input type="email" name="email">
-            <label>Enter password</label>
-            <input type="password" name="password">
-            <label>Confirm password</label>
-            <input type="password" name="password_confirmation">
-            <button type="button" class="btn btn-info">Previous</button>
-            <button class="btn btn-outline-dark">Next</button>
+            <!-- Step Wise Form Content -->
+            <form id="registration_form" action="{{ route('register') }}" method="POST" enctype="multipart/form-data">
+                @csrf
+
+                <!-- Step 1 Content -->
+                <section id="step-1" class="form-step">
+                    <h3>Choose your registration type</h3>
+                    <!-- Step 1 input fields -->
+                    <div class="d-flex">
+                        <div>
+                            <p>I want to bid on items!</p>
+                            <input type="radio" class="radio_item" value="bidder" name="usertype" id="usertype_bidder">
+                            <label class="label_item gentle-hover-shake" for="usertype_bidder"> <i
+                                    class="fa-solid fa-bag-shopping"></i> </label>
+                        </div>
+                        <div>
+                            <p>I have items to sell!</p>
+                            <input type="radio" class="radio_item" value="merchant" name="usertype"
+                                id="usertype_merchant">
+                            <label class="label_item gentle-hover-shake" for="usertype_merchant"> <i
+                                    class="fa-solid fa-tag"></i>
+                            </label>
+                        </div>
+                        <div>
+                            <p>I want to expertise items!</p>
+                            <input type="radio" class="radio_item" value="auctioneer" name="usertype"
+                                id="usertype_auctioneer">
+                            <label class="label_item gentle-hover-shake" for="usertype_auctioneer"> <i
+                                    class="fa-solid fa-user-tie"></i> </label>
+                        </div>
+                    </div>
+                    <div>
+                        <button class="button btn-navigate-form-step" type="button" step_number="2">Next</button>
+                    </div>
+                </section>
+
+                <!-- Step 2 Content, default hidden on page load. -->
+                <section id="step-2" class="form-step d-none">
+                    <h3>Set up your account credentials</h3>
+                    <!-- Step 2 input fields -->
+                    <div>
+                        <label>Enter a username</label>
+                        <input type="text" name="username">
+                        <label>Enter email</label>
+                        <input type="email" name="email">
+                        <label>Enter password</label>
+                        <input type="password" name="password">
+                        <label>Confirm password</label>
+                        <input type="password" name="password_confirmation">
+                    </div>
+                    <div>
+                        <button class="button btn-navigate-form-step" type="button" step_number="1">Prev</button>
+                        <button class="button btn-navigate-form-step" type="button" step_number="3">Next</button>
+                    </div>
+                </section>
+
+                <!-- Step 3 Content, default hidden on page load. -->
+                <section id="step-3" class="form-step d-none">
+                    <h3>Add your personal details</h3>
+                    <!-- Step 3 input fields -->
+                    <div>
+                        <label>First name</label>
+                        <input type="text" name="first_name">
+                        <label>Last name</label>
+                        <input type="text" name="last_name">
+                        <label>Birth date</label>
+                        <input type="date" name="birth_date">
+                        <label>Contact number</label>
+                        <input type="text" name="contact_number">
+                    </div>
+                    <div>
+                        <button class="button btn-navigate-form-step" type="button" step_number="2">Prev</button>
+                        <button class="button btn-navigate-form-step" type="button" step_number="4">Next</button>
+                    </div>
+                </section>
+
+                <!-- Step 4 Content, default hidden on page load. -->
+                <section id="step-4" class="form-step d-none">
+                    <h3>Add your location details</h3>
+                    <!-- Step 4 input fields -->
+                    <div>
+                        <label>Street</label>
+                        <input type="text" name="street">
+                        <label>City</label>
+                        <input type="text" name="city">
+                        <label>Postal/Zip Code</label>
+                        <input type="text" name="postal_code">
+                        <label>Country</label>
+                        <input type="text" name="country">
+                    </div>
+                    <div>
+                        <button class="button btn-navigate-form-step" type="button" step_number="3">Prev</button>
+                        <button class="button btn-navigate-form-step" type="button" step_number="5">Next</button>
+                    </div>
+                </section>
+
+                <!-- Step 5 Content, default hidden on page load. -->
+                <section id="step-5" class="form-step d-none">
+                    <h3>Confirm your registration</h3>
+                    <!-- Step 5 input fields -->
+                    <div>
+                        <p>Agree to terms and conditions</p>
+                        <p>Success of registration</p>
+                        <p>Case of auctioneer, need to wait for admin approval</p>
+                    </div>
+                    <div>
+                        <button class="button btn-navigate-form-step" type="button" step_number="4">Prev</button>
+                        <button class="button submit-btn" type="submit">Save</button>
+                    </div>
+                </section>
+
+            </form>
+
         </div>
 
-        <!-- Step 3: personal information -->
-         <div class="step card" id="Personal_Information_Content">
-            <h3>Step 3 out of 4: Add personal details</h3>
-            <label>First name</label>
-            <input type="text" name="first_name"> 
-            <label>Last name</label>
-            <input type="text" name="last_name">
-            <label>Birth date</label>
-            <input type="date" name="birth_date">
-            <label>Contact number</label>
-            <input type="text" name="contact_number">
-            <button type="button" class="btn btn-info">Previous</button>
-            <button class="btn btn-outline-dark">Next</button>
-        </div> 
+    </div>
 
-        <!-- Step 4: location information -->
-         <div class="step card" id="Location_Information_Content">
-            <h3>Step 4 out of 4: Add location details</h3>
-            <label>Street</label>
-            <input type="text" name="street">
-            <label>City</label>
-            <input type="text" name="city">
-            <label>Postal/Zip Code</label>
-            <input type="text" name="postal_code">
-            <label>Country</label>
-            <input type="text" name="country">
-            <button type="button" class="btn btn-info">Previous</button>
-            <button class="btn btn-outline-dark">Next</button>
-        </div> 
-
-        <!-- Confirming user registration -->
-        <div class="step card" id="Confirmation">
-            <p>Agree to terms and conditions</p>
-            <p>Success of registration</p>
-            <p>Case of auctioneer, need to wait for admin approval</p>
-            <button type="button" class="btn btn-info">Previous</button>
-            <input class="btn btn-outline-dark" type="submit" value="Confirm">
-        </div>
-
-    </form>
-
-    {{-- @vite(['resources/js/register.js']) --}}
+    @vite(['resources/js/register.js'])
 @endsection
