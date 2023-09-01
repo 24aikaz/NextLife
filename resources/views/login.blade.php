6b0372d451815095d0b8e6489c70b742e28e9703
Keea <zak.owasil@gmail.com>
2023-09-01 20:09:45 +0400

    Fixes to Login UI (Almost done)

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -3,21 +3,21 @@
 @section('content')
     <div id="login_container">
 
-        <h2>Log In</h2>
-
         <form id="LogIn_Form" action="{{ route('login') }}" method="POST">
 
             @csrf
 
             <div id="LogIn_Form_Content" class="card">
 
+                <h2 class="title text-center">Log in</h2>
+
                 @if (session('status'))
-                    <div class="errormsg">
-                        {{ session('status') }}
+                    <div class="loginfail">
+                        <i class="fa-solid fa-triangle-exclamation"></i> {{ session('status') }}
                     </div>
                 @endif
 
-                <Label for="login_username">Username:</Label>
+                <Label class="toppad" for="login_username">Username:</Label>
                 <input name="username" id="login_username" type="text" value="{{ old('username') }}"
                     placeholder="Enter your username">
                 @error('username')
@@ -26,7 +26,7 @@
                     </div>
                 @enderror
 
-                <Label for="login_pwd">Password:</Label>
+                <Label class="toppad" for="login_pwd">Password:</Label>
                 <input name="password" id="login_pwd" type="password" value="{{ old('password') }}"
                     placeholder="Enter your password">
                 @error('password')
@@ -35,15 +35,14 @@
                     </div>
                 @enderror
 
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" checked>
+                <div class="form-check toppad">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" unchecked title="We will remember you next time you come!">
                     <label for="remember">Remember me</label>
                 </div>
 
-                {{-- <input id="login_btn" class="btn" type="submit" value="Log In"> --}}
                 <button id="login_btn" class="btn" type="submit">Log In</button>
 
-                <p>No account yet? <a class="clickable_stuff" href="{{ route('register') }}">Register Now</a></p>
+                <p class="text-center">No account yet? <a class="clickable_stuff registernow" href="{{ route('register') }}">Register Now</a></p>
 
             </div>
 
