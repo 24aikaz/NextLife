2cdc766558bcea5eca7bdf0d8739fdd9d61c6100
Keea <zak.owasil@gmail.com>
2023-09-06 14:37:41 +0400

    Fixes to product view

--- resources/views/paymentmethod.blade.php
+++ resources/views/paymentmethod.blade.php
@@ -1,22 +1,28 @@
 @extends('app')
 
 @section('content')
-<section>  
+    <div id="payment_content">
 
-    <h2>Select Payment Method:</h2>
-    <form method="post" action="{{ route('checkout') }}">
-        @csrf 
-        <label for="paymentMethodVisa">Visa Card</label>
-        <input type="radio" id="paymentMethodVisa" name="payment_method" value="Visa Card" required>
-        <br>
-        <label for="paymentMethodPayPal">PayPal</label>
-        <input type="radio" id="paymentMethodPayPal" name="payment_method" value="PayPal" required>
-        <br>
-        <!-- Add more payment methods if needed -->
-        <input type="submit" value="Submit">
-    </form>
-</section>
+        <div class="payment_title_card card mx-auto">
+            <h2 class="title text-center">Payment</h2>
+        </div>
 
+        <form method="post" action="{{ route('checkout') }}">
+            @csrf
 
-   @vite(['resources/js/viewproduct.js']) 
-  @endsection
+            <label for="paymentMethodVisa">Visa Card</label>
+            <input class="form-check" type="radio" id="paymentMethodVisa" name="payment_method" value="Visa Card" required>
+            <br>
+
+            <label for="paymentMethodPayPal">PayPal</label>
+            <input class="form-check" type="radio" id="paymentMethodPayPal" name="payment_method" value="PayPal" required>
+            <br>
+
+            <!-- Add more payment methods if needed -->
+            <input class="btn btn-outline-dark" type="submit" value="Submit">
+        </form>
+
+    </div>
+
+    @vite(['resources/js/payment.js'])
+@endsection
--- resources/views/viewproduct.blade.php
+++ resources/views/viewproduct.blade.php
@@ -1,23 +1,33 @@
 @extends('app')
 
 @section('content')
-    <div id="viewproduct_content" style="padding-top: 5rem">
-        <h2>Details about the product</h2>
-        <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->pname }}" class="product-image">
-        <h4>{{ $product->pname }}</h4>
-        <p>Description: {{ $product->pdesc }}</p>
-        <p>Starting bid: ${{ $product->startprice }}</p>
-        <p>Current bid: ${{ $product->currentprice }}</p>
-        <p>Bids placed: {{ $product->bidcount }}</p>
-        <p>Category: {{ $product->category }}</p>
-        <p>End date: {{ $product->enddate }}</p>
+    <div id="viewproduct_content">
 
-        <form action="{{ route('place-bid', ['id' => $product->Product_ID]) }}" method="POST">
-            @csrf
-            <input type="number" name="bid_price" class="form-control" placeholder="Enter bid price">
-            <button type="submit" class="btn btn-primary">Place Bid</button>
-        </form>
+        <div class="row row-cols-2">
+
+            <div class="col">
+                <img src="{{ asset('storage/' . $product->image) }}" alt="{{ $product->pname }}" class="product_image">
+            </div>
+
+            <div class="col">
+                <h4>{{ $product->pname }}</h4>
+                <p>Description: {{ $product->pdesc }}</p>
+                <p>Starting bid: ${{ $product->startprice }}</p>
+                <p>Current bid: ${{ $product->currentprice }}</p>
+                <p>Bids placed: {{ $product->bidcount }}</p>
+                <p>Category: {{ $product->category }}</p>
+                <p>End date: {{ $product->enddate }}</p>
+
+                <form action="{{ route('place-bid', ['id' => $product->Product_ID]) }}" method="POST">
+                    @csrf
+                    <input type="number" name="bid_price" class="form-control" placeholder="Enter bid price">
+                    <button type="submit" class="btn btn-outline-dark">Place Bid</button>
+                </form>
+            </div>
+
+        </div>
+
     </div>
 
-    {{-- @vite(['resources/js/viewproduct.js']) --}}
+    @vite(['resources/js/viewproduct.js'])
 @endsection
